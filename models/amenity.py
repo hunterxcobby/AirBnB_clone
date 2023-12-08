da29869b8eeeeb41fd68bc39225f64a512a21b9b
hunterxcobby <solomonsefah13@gmail.com>
2023-12-08 20:56:16 +0000

    an amenity module that inherits from the superclass basemodel

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Amenity Module:
+Inherits from Superclass BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel."""
+    name: str = ""
