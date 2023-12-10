e2e354a258e16bcefe5d4bd24e5d3923e18d925a
Ami Manye <amimanyeaglago@gmail.com>
2023-12-10 10:19:40 +0000

    created and imported unittest mod

---
+++ test/test_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+This module is a test file used to test the BaseModel class of our AirBnB_clone project
+'''
+
+# modules that will be used here
+
+import unittest
+
+# Test Cases
+
+class TestBaseModel(unittest.TestCase):
+
