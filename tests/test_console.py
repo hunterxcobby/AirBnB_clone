3d0de6b15e1fd8e2c5206be6e431f215b384c95e
aglago <samuellamanyeaglago@gmail.com>
2023-12-10 21:56:20 +0000

    fix pycodestyle:exceeds 80 characters

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,60 +1,99 @@
 #!/usr/bin/python3
 
+"""Unnitest for console
+"""
+
+import sys
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
-from models import storage
 from models.base_model import BaseModel
+from models import storage
 
-class TestHBNBCommand(unittest.TestCase):
+
+class TestConsole(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.console = HBNBCommand()
 
     def setUp(self):
-        """Set up a clean slate for each test."""
-        storage._FileStorage__objects = {}
+        self.console.preloop()
 
     def tearDown(self):
-        """Clean up after each test."""
-        storage._FileStorage__file_path = "file.json"
-        storage._FileStorage__objects = {}
+        self.console.postloop()
 
-    def assert_stdout(self, command, expected_output):
-        with patch('sys.stdout', new=StringIO()) as mock_stdout:
-            HBNBCommand().onecmd(command)
-            self.assertIn(expected_output, mock_stdout.getvalue())
+    @patch('sys.stdout', new_callable=StringIO)
+    def assert_stdout(self, expected_output, mock_stdout, command):
+        self.console.onecmd(command)
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_create_command(self):
-        self.assert_stdout("create BaseModel", "BaseModel")
+    def test_create_show_destroy_all_update_commands(self):
+        storage.reset()
 
-    def test_show_command(self):
-        storage._FileStorage__objects['BaseModel.1'] = BaseModel()
-        self.assert_stdout("show BaseModel 1", "BaseModel")
+        # Create
+        expected_output = "f1r57-1n574nc3\n"
+        self.assert_stdout(expected_output, "create BaseModel")
+        obj = storage.all()['BaseModel.f1r57-1n574nc3']
 
-    def test_destroy_command(self):
-        storage._FileStorage__objects['BaseModel.1'] = BaseModel()
-        self.assert_stdout("destroy BaseModel 1", "")
+        # Show
+        expected_output = str(obj) + '\n'
+        self.assert_stdout(expected_output, "show BaseModel {}".format(obj.id))
 
-    def test_all_command(self):
-        storage._FileStorage__objects['BaseModel.1'] = BaseModel()
-        self.assert_stdout("all BaseModel", "BaseModel")
+        # Destroy
+        self.assert_stdout("", "destroy BaseModel {}".format(obj.id))
+        self.assertNotIn(obj, storage.all().values())
 
-    def test_update_command(self):
-        storage._FileStorage__objects['BaseModel.1'] = BaseModel()
-        self.assert_stdout("update BaseModel 1 name 'NewName'", "")
+        # All
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        expected_output = "[{}, {}]\n".format(str(obj1), str(obj2))
+        self.assert_stdout(expected_output, "all BaseModel")
 
-    def test_custom_command(self):
-        storage._FileStorage__objects['BaseModel.1'] = BaseModel()
-        self.assert_stdout("BaseModel.show(1)", "BaseModel")
+        # Update
+        self.assert_stdout(
+            "",
+            'update BaseModel {} name "New Name"'.format(obj1.id)
+        )
+        updated_obj = storage.all()['BaseModel.' + obj1.id]
+        self.assertEqual(updated_obj.name, "New Name")
 
-    def test_invalid_command(self):
-        self.assert_stdout("invalidCommand", "Unrecognized command:")
+    def test_invalid_commands(self):
+        storage.reset()
 
-    def test_emptyline_command(self):
-        self.assert_stdout("", "")
+        # Invalid create command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "create InvalidClass")
 
-    def test_EOF_command(self):
-        with self.assertRaises(SystemExit):
-            self.assert_stdout("EOF", "")
+        # Invalid show command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "show InvalidClass")
+
+        # Invalid destroy command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "destroy InvalidClass")
+
+        # Invalid all command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "all InvalidClass")
+
+        # Invalid update command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "update InvalidClass")
+
+    def test_empty_line_quit_commands(self):
+        # Empty line
+        expected_output = ""
+        self.assert_stdout(expected_output, "")
+
+        # Quit command
+        self.assertTrue(self.console.onecmd("quit"))
+
+    def test_help_commands(self):
+        expected_output = "Quit command to exit the program\n"
+        self.assert_stdout(expected_output, "help quit")
+
 
 if __name__ == '__main__':
     unittest.main()
