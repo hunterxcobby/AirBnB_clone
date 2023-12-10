eb0f2c1f7681d8113a96a52cdcab8e682b073b03
Ami Manye <amimanyeaglago@gmail.com>
2023-12-10 10:51:10 +0000

    checking for string uuids

--- test/test_base_model.py
+++ test/test_base_model.py
@@ -7,8 +7,28 @@ This module is a test file used to test the BaseModel class of our AirBnB_clone 
 # modules that will be used here
 
 import unittest
+from models.base_model import BaseModel
+
+# helper functions
+
+def is_string(value):
+    ''' return true if value is string '''
+    return isintance(value)
 
 # Test Cases
 
 class TestBaseModel(unittest.TestCase):
+    '''
+    This is a test case that is testing all cases in the BaseModel class of our project
+    '''
 
+    # testing uuid of every instance
+
+    def test_uuid(self):
+        ''' uuids must be equal no matter what '''
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+
+        # uuids are string
+        self.assertTrue(is_string(obj1.id))
+        self.assertTrue(is_string(obj2.id))
